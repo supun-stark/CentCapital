f22e53c615594638e6faf6828fdc729a1d04755a
supun-stark <it19234694@my.sliit.lk>
2024-08-08 12:51:13 +0530

    Create right sidebar and routes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 
 const Home = () => {
 
-  const loggedIn = { firstName: 'John'};
+  const loggedIn = { firstName: 'John', lastName: 'Doe', email: 'johndoe@gmail.com'};
 
   return (
     <section className='home'>
@@ -25,6 +26,11 @@ const Home = () => {
             />
         </header>
       </div>
+      <RightSidebar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 1780.40}, {currentBalance: 500.69}]}
+          />
     </section>
   )
 }
