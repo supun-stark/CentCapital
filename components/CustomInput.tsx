3469191b373f2a91baa35d7575d0fb45913d459b
supun-stark <it19234694@my.sliit.lk>
2024-08-10 14:41:58 +0530

    Create Sign in page with custom input fields

---
+++ components/CustomInput.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Input } from './ui/input'
+import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
+
+import { Control, FieldPath } from 'react-hook-form'
+import { z } from 'zod'
+import { authFormSchema } from '@/lib/utils'
+
+interface CustomInput {
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: FieldPath<z.infer<typeof authFormSchema>>,
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({control, name, label, placeholder}: CustomInput) => {
+  return (
+                  <FormField          
+                            control={control}
+                            name={name}
+                            render={({ field }) => (
+                                <div className='form-item'>
+                                    <FormLabel className='form-label'>
+                                        {label}
+                                    </FormLabel>
+                                    <div className="flex w-full flex-col">
+                                        <FormControl>
+                                            <Input 
+                                                placeholder={placeholder}
+                                                className='input-class'
+                                                type={name === 'password' ? 'password':'text'}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage
+                                           className='mt-2 form-message'
+                                        />
+
+                                    </div>
+                                </div>
+
+                            )}
+                        />
+  )
+}
+
+export default CustomInput
